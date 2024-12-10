30568e2f59868237a203e90fa91ffb1ab34c6be2
Younan <younangamal722@gmail.com>
2024-12-10 23:02:31 +0200

    + create home repo + create home repo methods and failure file

---
+++ lib/features/home/data/repos/home_repo.dart
@@ -0,0 +1,9 @@
+import 'package:bookly_app/core/errors/failure.dart';
+import 'package:bookly_app/features/home/data/models/booke_model/booke_model.dart';
+import 'package:dartz/dartz.dart';
+
+abstract class HomeRepo {
+  Future<Either<Failure, List<BookModel>>> fetchBestSellerBooks();
+
+  Future<Either<Failure, List<BookModel>>> fetchFeatureBooks();
+}
