69b7c7b7bd98c3aa1540dc53a394d963674cb015
Younan <younangamal722@gmail.com>
2024-12-13 13:57:12 +0200

    +create newset books method

--- lib/features/home/presentation/manager/newset_books_cubit/newset_books_cubit.dart
+++ lib/features/home/presentation/manager/newset_books_cubit/newset_books_cubit.dart
@@ -1,9 +1,25 @@
 import 'package:bloc/bloc.dart';
 import 'package:bookly_app/features/home/data/models/booke_model/booke_model.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo.dart';
 import 'package:meta/meta.dart';
 
 part 'newset_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit() : super(NewsetBooksInitial());
+  NewsetBooksCubit(this.homeRepo) : super(NewsetBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewsetBooks() async {
+    emit(NewsetBooksLoading());
+    var result = await homeRepo.fetchNewSetBooks();
+    result.fold(
+      (failure) {
+        emit(NewsetBooksFailure(failure.errorMessage));
+      },
+      (books) {
+        emit(NewsetBooksSuccess(books));
+      },
+    );
+  }
 }
